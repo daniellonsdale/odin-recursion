628faa05eedbfb83365ecdee95b81d08b90a43e3
daniellonsdale <danielllonsdale@gmail.com>
2025-04-14 21:22:43 +0900

    Fix algo

--- fibonacci.js
+++ fibonacci.js
@@ -5,7 +5,7 @@ function fibs(num){
         return [0, 1];
     }else{
         let arr = [0, 1];
-        for (let i = 2; i <= num; i++) {
+        for (let i = 2; i < num; i++) {
             arr[i] = arr[i-2] + arr[i-1];
         }
         return arr;
