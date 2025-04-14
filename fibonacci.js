8b043ac8f8790555ffc69ffef61925af25a3f572
daniellonsdale <danielllonsdale@gmail.com>
2025-04-14 21:18:21 +0900

    Complete first iterative attempt

--- fibonacci.js
+++ fibonacci.js
@@ -4,6 +4,10 @@ function fibs(num){
     }else if (num == 1){
         return [0, 1];
     }else{
-        
+        let arr = [0, 1];
+        for (let i = 2; i <= num; i++) {
+            arr[i] = arr[i-2] + arr[i-1];
+        }
+        return arr;
     }
 }
