1f5e141b4a1589f047f9a60c7030189eed63b114
daniellonsdale <danielllonsdale@gmail.com>
2025-04-23 23:44:49 +0900

    Add explanation comments

--- mergesort.js
+++ mergesort.js
@@ -1,10 +1,14 @@
 function mergesort(arr){
+    //Base case
     if (arr.length <= 1){
         return arr;
     }else{
+        //Create arrays for merging
         let newArrLeft = [];
         let newArrRight = [];
         let newArr = [];
+
+        //Call recursive function to split array until length <= 1 (base case)
         if(arr.length % 2 == 0){
             newArrLeft = mergesort(arr.slice(0, arr.length/2));
             newArrRight = mergesort(arr.slice(arr.length/2, arr.length));
@@ -12,9 +16,11 @@ function mergesort(arr){
             newArrLeft = mergesort(arr.slice(0, Math.ceil(arr.length/2)));
             newArrRight = mergesort(arr.slice(Math.ceil(arr.length/2)), arr.length);
         }
+
         let curLeftPos = 0;
         let curRightPos = 0;
 
+        //Loop through as long as the are elements in both arrays and compare to build sorted array
         while(curLeftPos < newArrLeft.length && curRightPos < newArrRight.length){
             if (newArrLeft[curLeftPos] <= newArrRight[curRightPos]){
                 newArr.push(newArrLeft[curLeftPos]);
@@ -24,6 +30,7 @@ function mergesort(arr){
                 curRightPos++;
             }
         }
+        //Add all elements from remaining side since they must be already sorted and greater
         while(curLeftPos < newArrLeft.length){
             newArr.push(newArrLeft[curLeftPos]);
             curLeftPos++;
